4f56dfe058f45260af7b0280913abe40b37a0fa4
didin2003 <didinpg8080@gmail.com>
2025-11-11 15:53:42 +0530

    Move route and resource loading to boot method

--- Modules/Pricing/Providers/PricingServiceProvider.php
+++ Modules/Pricing/Providers/PricingServiceProvider.php
@@ -10,16 +10,14 @@ class PricingServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        // $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
-        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'pricing');
-        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
-
         $this->app->bind(PricingRepositoryInterface::class, PricingRepository::class);
     }
 
     public function boot(): void
     {
-        //
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        // $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'pricing');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
     }
 }
