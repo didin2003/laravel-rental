0e280fa1e570b42bbbe53db112632cd4f6f6792b
didin2003 <didinpg8080@gmail.com>
2025-11-11 17:14:58 +0530

    Fix case sensitivity in route file path

--- Modules/Nearby/Providers/NearbyServiceProvider.php
+++ Modules/Nearby/Providers/NearbyServiceProvider.php
@@ -15,7 +15,7 @@ class NearbyServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
         // $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
         $this->loadViewsFrom(__DIR__.'/../Resources/views', 'nearby');
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
