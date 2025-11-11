9b2ba0cf4e2c7cfb1f0acca93c9ab78b8397f8e3
didin2003 <didinpg8080@gmail.com>
2025-11-11 17:08:47 +0530

    Fix route file casing in AmenityServiceProvider

--- Modules/Amenity/Providers/AmenityServiceProvider.php
+++ Modules/Amenity/Providers/AmenityServiceProvider.php
@@ -17,7 +17,7 @@ class AmenityServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../Resources/views', 'amenity');
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
     }
