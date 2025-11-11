fd4b9dfee87a798bb9b5ef383c988e8c77673d3b
Didin <didinpg8080@gmail.com>
2025-11-11 10:19:28 +0000

    test

--- Modules/Amenity/Providers/AmenityServiceProvider.php
+++ Modules/Amenity/Providers/AmenityServiceProvider.php
@@ -12,14 +12,13 @@ class AmenityServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'amenity');
-        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
         $this->app->bind(AmenityRepositoryInterface::class, AmenityRepository::class);
     }
 
     public function boot(): void
     {
-        //
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'amenity');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
     }
 }
