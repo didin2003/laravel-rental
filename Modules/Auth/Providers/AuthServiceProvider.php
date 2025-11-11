42047f333e1f536413267caf468e6fec97bdd78a
didin2003 <didinpg8080@gmail.com>
2025-11-11 17:45:21 +0530

    Update AuthServiceProvider.php

--- Modules/Auth/Providers/AuthServiceProvider.php
+++ Modules/Auth/Providers/AuthServiceProvider.php
@@ -17,7 +17,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'auth');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'auth');
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
     }
 }
