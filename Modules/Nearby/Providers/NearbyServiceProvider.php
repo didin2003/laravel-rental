251c613314c877790e95ffdb413e998ad018bd3d
didin2003 <didinpg8080@gmail.com>
2025-11-11 15:52:54 +0530

    Update NearbyServiceProvider.php

--- Modules/Nearby/Providers/NearbyServiceProvider.php
+++ Modules/Nearby/Providers/NearbyServiceProvider.php
@@ -10,16 +10,14 @@ class NearbyServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        // $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
-        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'nearby');
-        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
-
         $this->app->bind(NearbyRepositoryInterface::class, NearbyRepository::class);
     }
 
     public function boot(): void
     {
-        //
+        this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        // $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'nearby');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
     }
 }
