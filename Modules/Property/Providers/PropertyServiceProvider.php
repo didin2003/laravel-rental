f748b02c5ac96f7ae8c739301ad1930b08bb5f54
didin2003 <didinpg8080@gmail.com>
2025-11-11 15:54:18 +0530

    Move route and resource loading to boot method

--- Modules/Property/Providers/PropertyServiceProvider.php
+++ Modules/Property/Providers/PropertyServiceProvider.php
@@ -12,17 +12,17 @@ class PropertyServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
-        // $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
-        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'property');
-        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
-
         $this->app->bind(PropertyRepositoryInterface::class, PropertyRepository::class);
 
     }
 
     public function boot(): void
     {
+         $this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
+        // $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'property');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
+
         Route::model('property', Property::class);
         Route::bind('property', function ($value) {
             return Property::where('slug', $value)->sole();
